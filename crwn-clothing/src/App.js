60a1f36548dacfa701940fb2da222990cb845db2
Quentin Delettre <quentin.delettre@pm.me>
2022-06-05 15:46:49 +0000

    feat: auth + firebase

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,14 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
