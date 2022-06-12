e49a5323047b7f80d4db5ff89cbc6e7fea0f9ae1
Quentin Delettre <quentin.delettre@pm.me>
2022-06-12 15:19:44 +0000

    feat: context

--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -9,6 +9,7 @@ import {
   signInWithPopup,
   signInWithRedirect,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -76,3 +77,5 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
   return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const signOutUser = async () => await signOut(auth);
