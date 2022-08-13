41052756b56117cd75b3e532c2f38ce3eee909d8
Quentin Delettre <quentin.delettre@pm.me>
2022-08-13 20:29:18 +0000

    feat: firebase database storage

---
+++ crwn-clothing/src/routes/category/category.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import ProductCard from "../../components/product-card/product-card.component";
+import { CategoriesContext } from "../../context/categories.context";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <div className="category-container">
+      {products &&
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+    </div>
+  );
+};
+
+export default Category;
