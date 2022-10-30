062c40ac67f4afb3d4a7801143ef6a8d7abeea9d
Quentin Delettre <quentin.delettre@pm.me>
2022-10-30 16:59:18 +0100

    feat: redux extended tools (#14)
    - chore: node 18
    - redux-enhanced-tools

--- crwn-clothing/src/store/store.js
+++ crwn-clothing/src/store/store.js
@@ -1,9 +1,33 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import { rootReducer } from "./root-reducer";
 import { logger } from "redux-logger";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composeEnhancers =
+  (process.env.NODE_ENV !== "production" &&
+    typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancers(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
