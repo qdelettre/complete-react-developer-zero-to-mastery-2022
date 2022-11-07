588c326ed806ec64083d0c379c735c7d19d5e50f
Quentin Delettre <quentin.delettre@pm.me>
2022-11-07 21:23:02 +0000

    serverless-functions-and-stripe
    Works with env variables
    - REACT_APP_STRIPE_PUBLISHABLE_KEY
    - STRIPE_SECRET_KEY

---
+++ crwn-clothing/src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,77 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import {
+  PaymentFormContainer,
+  FormContainer,
+  PaymentButton,
+} from "./payment-form.styles";
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../store/user/user.selectors";
+import { useState } from "react";
+
+const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
+  const [isProcessingPayment, setIsPRocessingPayment] = useState(false);
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsPRocessingPayment(true);
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: amount * 100 }),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: currentUser ? currentUser.displayName : "Guest",
+        },
+      },
+    });
+
+    setIsPRocessingPayment(false);
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("ok");
+      }
+    }
+  };
+
+  return (
+    <PaymentFormContainer>
+      <FormContainer onSubmit={paymentHandler}>
+        <h2>Credit Card Payment:</h2>
+        <CardElement />
+        <PaymentButton
+          isLoading={isProcessingPayment}
+          buttonType={BUTTON_TYPE_CLASSES.inverted}
+        >
+          Pay now
+        </PaymentButton>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
+
+export default PaymentForm;
